ba6a2fc6156d50aaa7ff9d5adcc9be21adeb7450
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-05-27 18:40:48 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/hhanri/rando_point_deau/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hhanri.rando_point_deau
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Rando Point d'eau",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
