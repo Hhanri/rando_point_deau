421d25a2a7c239a505ca7d465b8ce2ff79c3c48f
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-04 14:01:12 +0200

    [map] registered map feature inside setup container

--- lib/config/setup_container.dart
+++ lib/config/setup_container.dart
@@ -1,6 +1,15 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:rando_point_deau/core/db/sqflite_setup.dart';
+import 'package:rando_point_deau/core/wrappers/cached_stream.dart';
+import 'package:rando_point_deau/features/map/data/data_sources/map_data_source_interface.dart';
+import 'package:rando_point_deau/features/map/data/data_sources/map_local_data_source.dart';
+import 'package:rando_point_deau/features/map/data/repository/map_repository_implementation.dart';
+import 'package:rando_point_deau/features/map/domain/repository/map_repository_interface.dart';
+import 'package:rando_point_deau/features/map/domain/use_cases/map_get_filters_use_case.dart';
+import 'package:rando_point_deau/features/map/domain/use_cases/map_set_filters_use_case.dart';
+import 'package:rando_point_deau/features/map/presentation/cubits/map_cubit/map_cubit.dart';
+import 'package:rando_point_deau/features/map/presentation/cubits/map_filters_cubit/map_filters_cubit.dart';
 import 'package:rando_point_deau/features/onboarding/presentation/cubits/onboarding_cubit/onboarding_cubit.dart';
 import 'package:rando_point_deau/features/water/data/data_sources/owater_water_data_source.dart';
 import 'package:rando_point_deau/features/water/data/data_sources/sqfite_water_data_source.dart';
@@ -12,6 +21,7 @@ import 'package:rando_point_deau/features/water/domain/use_cases/water_download_
 import 'package:rando_point_deau/features/water/domain/use_cases/water_has_local_data_use_case.dart';
 import 'package:rando_point_deau/features/water/domain/use_cases/water_search_use_case.dart';
 import 'package:rando_point_deau/features/water/presentation/cubits/water_downloader_cubit/water_downloader_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 
 final sl = GetIt.instance;
@@ -23,6 +33,8 @@ Future<void> setupSL() async {
     "rando_point_deau.db",
     options: SQFliteConfig.dbV1,
   );
+
+  final prefs = await SharedPreferences.getInstance();
 
   sl.registerLazySingleton<WaterRemoteDataSourceInterface>(
     () => OwaterWaterDataSource(httpClient),
@@ -61,6 +73,38 @@ Future<void> setupSL() async {
   sl.registerFactory<OnboardingCubit>(
     () => OnboardingCubit(
       waterHasLocalDataUseCase: sl.get<WaterHasLocalDataUseCase>(),
+    )..init(),
+  );
+
+  sl.registerLazySingleton<MapDataSourceInterface>(
+    () => MapLocalDataSource(prefs),
+  );
+  sl.registerLazySingleton<MapRepositoryInterface>(
+    () => MapRepositoryImplementation(
+      sl.get<MapDataSourceInterface>(),
+    ),
+  );
+  sl.registerLazySingleton<MapSetFiltersUseCase>(
+    () => MapSetFiltersUseCase(
+      sl.get<MapRepositoryInterface>(),
+    ),
+  );
+  sl.registerLazySingleton<MapGetFiltersUseCase>(
+    () => MapGetFiltersUseCase(
+      sl.get<MapRepositoryInterface>(),
+    ),
+  );
+
+  sl.registerFactory<MapFiltersCubit>(
+    () => MapFiltersCubit(
+      getFiltersUseCase: sl.get<MapGetFiltersUseCase>(),
+      setFiltersUseCase: sl.get<MapSetFiltersUseCase>(),
+    )..init(),
+  );
+  sl.registerFactoryParam<MapCubit, MapFiltersCubit, void>(
+    (p1, _) => MapCubit(
+      waterSearchUseCase: sl.get<WaterSearchUseCase>(),
+      filtersStateStream: CachedStream(p1.stream),
     )..init(),
   );
 }
